2869efc2e9be487246ebb587464a27920f2e9ea9
Phil Johnston <phil.johnston@wpengine.com>
2024-09-03 13:52:03 -0400

    Try setting headers another way

--- service-worker.js
+++ service-worker.js
@@ -25,16 +25,7 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request).then((response) => {
-          const newHeaders = new Headers(response.headers);
-          newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
-          newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-          return new Response(response.body, {
-              status: response.status,
-              statusText: response.statusText,
-              headers: newHeaders,
-          });
-        })
+        return fetch(event.request);
       })
   );
 });
